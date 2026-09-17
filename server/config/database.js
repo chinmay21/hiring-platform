1e12a936ce8a67571d0ddeb20c3eac362b6f9bf3
Chinmay <dhaundiyalchinmay@gmail.com>
2026-09-17 23:00:15 +0530

    Initial commit

---
+++ server/config/database.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const dbConnect = () => {
+    mongoose.connect(process.env.DATABASE_URL)
+    .then(() => console.log("Database connected successfully"))
+    .catch((error) => {
+        console.log("Error in database connection")
+        process.exit(1)
+    })
+}
+
+module.exports = dbConnect;
---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+require("dotenv").config();
+const dbConnect = require('./config/database');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+dbConnect();
+
+app.get('/', (req, res) => {
+    return res.json({
+        success: true,
+        message: "Server started successfully"
+    })
+});
+
+app.listen(PORT, () => {
+    console.log(`App is running at PORT ${PORT}`)
+});
