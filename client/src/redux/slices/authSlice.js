bef1e3cd54a6901d82f20fde4fab6f8339b9f43b
Chinmay <dhaundiyalchinmay@gmail.com>
2026-09-25 22:38:26 +0530

    Set up Redux state management

---
+++ client/src/redux/slices/authSlice.js
@@ -0,0 +1,36 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    signupData: null,
+    loading: false,
+    user: localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")) : null,
+    token: localStorage.getItem("token") ? JSON.parse(localStorage.getItem("token")) : null
+}
+
+const authSlice = createSlice({
+    name:"auth",
+    initialState:initialState,
+    reducers:{
+        setSignupData(state, value) {
+            state.signupData = value.payload;
+        },
+        setUser(state, value) {
+            state.user = value.payload; 
+        },
+        setLoading(state, value) {
+            state.loading = value.payload;
+        },
+        setToken(state, value) {
+            state.token = value.payload;
+        },
+        logout(state) {
+            state.user = null;
+            state.token = null;
+            localStorage.removeItem("token");
+            localStorage.removeItem("user");
+        }
+    }
+});
+
+export const {setSignupData, setUser, setLoading, setToken, logout} = authSlice.actions;
+export default authSlice.reducer;
